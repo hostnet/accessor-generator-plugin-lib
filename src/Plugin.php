f57b88c821640b0db2c749d1c25aac1391c21863
Hidde Boomsma <hboomsma@hostnet.nl>
2014-11-06 09:35:35 +0100

    added Plugin

---
+++ src/Plugin.php
@@ -0,0 +1,153 @@
+<?php
+namespace Hostnet\Component\AccessorGenerator;
+
+use Composer\Composer;
+use Composer\EventDispatcher\EventSubscriberInterface;
+use Composer\IO\IOInterface;
+use Composer\Package\PackageInterface;
+use Composer\Package\RootPackageInterface;
+use Composer\Plugin\PluginInterface;
+use Composer\Script\ScriptEvents;
+use Doctrine\Common\Annotations\AnnotationRegistry;
+use Hostnet\Component\AccessorGenerator\Generator\CodeGenerator;
+use Hostnet\Component\AccessorGenerator\Generator\CodeGeneratorInterface;
+use Hostnet\Component\AccessorGenerator\Reflection\ReflectionClass;
+use Symfony\Component\Finder\Finder;
+
+/**
+ * Plugin that will generate accessor methods in traits put
+ * in a Generated folder and namespace relative for the file
+ * the trait is created for.
+ *
+ * The trait is included by hand in the file using the accessor-
+ * generation.
+ *
+ * The plugin will ONLY generate traits for packages that directly
+ * require the package.
+ *
+ * For more information on usage please see README.md
+ *
+ * @author Hidde Boomsma <hboomsma@hostnet.nl>
+ */
+class Plugin implements PluginInterface, EventSubscriberInterface
+{
+
+    const NAME = 'hostnet/accessor-generator-plugin-lib';
+
+    private $composer;
+    private $io;
+    private $generator;
+
+    /**
+     * Initialize the annotation registry with composer
+     * as autoloader. Create a CodeGemerator if now was
+     * provided.
+     *
+     * @param CodeGeneratorInterface $generator
+     */
+    public function __construct(CodeGeneratorInterface $generator = null)
+    {
+        AnnotationRegistry::registerLoader('class_exists');
+
+        if ($generator) {
+            $this->generator = $generator;
+        } else {
+            $this->generator = new CodeGenerator();
+        }
+    }
+
+    /**
+     * @see \Composer\Plugin\PluginInterface::activate()
+     * @param Composer $composer
+     * @param IOInterface $io
+     */
+    public function activate(Composer $composer, IOInterface $io)
+    {
+        $this->composer = $composer;
+        $this->io       = $io;
+    }
+
+    /**
+     * @see Composer\EventDispatcher\EventSubscriberInterface::getSubscribedEvents
+     * @return int|string[][]
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            ScriptEvents::PRE_AUTOLOAD_DUMP => ['onPreAutoloadDump', 20 ],
+        ];
+    }
+
+    /**
+     * Gets called on the POST_AUTOLOAD_DUMP event
+     *
+     * Generate Traits for every package that requires
+     * this plugin and has php files with the @Generate
+     * annotation set on at least one property.
+     */
+    public function onPreAutoloadDump()
+    {
+        $local_repository = $this->composer->getRepositoryManager()->getLocalRepository();
+        $packages         = $local_repository->getPackages();
+        $packages[]       = $this->composer->getPackage();
+        foreach ($packages as $package) {
+            /* @var $package PackageInterface */
+            if (key_exists(self::NAME, $package->getRequires())) {
+                $this->generateTraitsForPackage($package);
+            }
+        }
+    }
+
+    /**
+     * Generate traits on disk for the given package.
+     * Will only do so when the package actually has
+     * the @Generate annotation set on at least one
+     * property.
+     *
+     * @param PackageInterface $package
+     */
+    private function generateTraitsForPackage(PackageInterface $package)
+    {
+        if ($this->io->isVerbose()) {
+            $this->io->write('Generating accessors for <info>' . $package->getPrettyName() .'</info>');
+        }
+
+        foreach ($this->getFilesForPackage($package) as $filename) {
+            $class = new ReflectionClass($filename);
+            if ($this->generator->writeTraitForClass($class) && $this->io->isVeryVerbose()) {
+                $this->io->write("  - generated acessors for <info>$filename</info>");
+            }
+        }
+    }
+
+    /**
+     * Find all the PHP files within a package.
+     *
+     * Excludes
+     *  - all files in VCS directories
+     *  - all files in vendor folders
+     *  - all files in Generated folders
+     *  - all hidden files
+     *
+     * @param PackageInterface $package
+     * @return Iterator
+     */
+    private function getFilesForPackage(PackageInterface $package)
+    {
+        if ($package instanceof RootPackageInterface) {
+            $path = '.';
+        } else {
+            $path = $this->composer->getInstallationManager()->getInstallPath($package);
+        }
+
+        $finder = new Finder();
+
+        return $finder
+        ->ignoreVCS(true)
+        ->ignoreDotFiles(true)
+        ->exclude(['vendor', 'Generated'])
+        ->name('*.php')
+        ->in($path)
+        ->getIterator();
+    }
+}
---
+++ test/PluginTest.php
@@ -0,0 +1,103 @@
+<?php
+namespace Hostnet\Component\AccessorGenerator;
+
+use Composer\Composer;
+use Composer\Config;
+use Composer\EventDispatcher\EventDispatcher;
+use Composer\Installer\InstallationManager;
+use Composer\IO\BufferIO;
+use Composer\IO\NullIO;
+use Composer\Package\Package;
+use Composer\Package\RootPackage;
+use Composer\Repository\RepositoryManager;
+use Composer\Repository\WritableArrayRepository;
+use Composer\Script\ScriptEvents;
+use Hostnet\Component\AccessorGenerator\Generator\CodeGeneratorInterface;
+use Symfony\Component\Console\Output\StreamOutput;
+
+/**
+ * @covers Hostnet\Component\AccessorGenerator\Plugin
+ * @author Hidde Boomsma <hboomsma@hostnet.nl>
+ */
+class PluginTest extends \PHPUnit_Framework_TestCase
+{
+    public function testActivate()
+    {
+        // Only a smoke test.
+        $plugin = new Plugin();
+        $plugin->activate($this->getMockComposer(), new NullIO());
+    }
+
+    public function testGetSubscribedEvents()
+    {
+        $this->assertEquals(
+            [ScriptEvents::PRE_AUTOLOAD_DUMP => ['onPreAutoloadDump', 20 ]],
+            Plugin::getSubscribedEvents()
+        );
+    }
+
+    public function testOnPreAutoloadDump()
+    {
+        // Efectivly change installation dir of root package.
+        chdir(__DIR__ . '/fixtures');
+
+        // Get fake generator
+        $generator = $this->getMock(CodeGeneratorInterface::class);
+
+        // Hit every fixture two times (ignore annotations).
+        $generator->expects($this->exactly(4))->method('writeTraitForClass')->willReturn(true);
+
+        $plugin = new Plugin($generator);
+        $plugin->activate(self::getMockComposer(), new BufferIO('', StreamOutput::VERBOSITY_VERY_VERBOSE));
+        $plugin->onPreAutoloadDump();
+
+    }
+
+
+    /**
+     * Build up a composer environment with:
+     * - a RepositoryManager with
+     *   - a Repository with
+     *     - a package that depends on Plugin::NAME
+     *     - a package that not depends on Plugin::NAME
+     * - a Mocked InstallationManager that will return
+     *   __DIR__/fixtures when asked for an installation path
+     * - a default event dispatcher
+     * - a root package that depends on Plugin::NAME
+     *
+     * all with NullIO and a default empty config
+     *
+     * @return \Composer\Composer
+     */
+    private function getMockComposer()
+    {
+        $config = new Config();
+        $io     = new NullIO();
+
+        $root_package = new RootPackage(Plugin::NAME, 0, 0);
+        $root_package->setRequires([Plugin::NAME => 0]);
+
+        $silly_package = new Package('TestEntity', 0, 0);
+        $package       = new Package('TestEntity', 0, 0);
+        $package->setRequires([Plugin::NAME => 0]);
+
+        $repository = new WritableArrayRepository();
+        $repository->addPackage($silly_package);
+        $repository->addPackage($package);
+
+        $repository_manager = new RepositoryManager($io, $config);
+        $repository_manager->setLocalRepository($repository);
+
+        $installation_manager = $this->getMock(InstallationManager::class);
+        $installation_manager->expects($this->any())->method('getInstallPath')->willReturn(__DIR__ . '/fixtures');
+
+        $composer = new Composer();
+        $composer->setPackage($root_package);
+        $composer->setConfig($config);
+        $composer->setRepositoryManager($repository_manager);
+        $composer->setInstallationManager($installation_manager);
+        $composer->setEventDispatcher(new EventDispatcher($composer, $io));
+
+        return $composer;
+    }
+}
