1d8ceded907b7d61f881944785fd74ffca212103
Maarten Steltenpool <msteltenpool@hostnet.nl>
2017-01-20 11:29:28 +0100

    Added docs for the KeyRegistry file.

--- test/Generator/fixtures/expected/KeyRegistry.php
+++ test/Generator/fixtures/expected/KeyRegistry.php
@@ -9,31 +9,61 @@ namespace Hostnet\Component\AccessorGenerator\Generator\fixtures\Generated;
  */
 class KeyRegistry
 {
+    /**
+     * @var array
+     */
     private static $public_key_paths = [
        'database.table.column' => '',
        'database.table.column_again' => '',
     ];
 
+    /**
+     * @var array
+     */
     private static $private_key_paths = [
        'database.table.column' => '',
        'database.table.column_again' => '',
     ];
 
+    /**
+     * Gets the file path where the public key is located for the given encryption alias.
+     *
+     * @return string
+     */
     public static function getPublicKeyPath($alias)
     {
         return self::$public_key_paths[$alias] ?? '';
     }
 
+    /**
+     * Gets the file path where the private key is located for the given encryption alias.
+     *
+     * @return string
+     */
     public static function getPrivateKeyPath($alias)
     {
         return self::$private_key_paths[$alias] ?? '';
     }
 
+    /**
+     * Add a public key file path for a specific alias manually. If the alias already
+     * exists, the existing path is overwritten.
+     *
+     * @param string $alias
+     * @param string $path
+     */
     public static function addPublicKeyPath($alias, $path)
     {
         self::$public_key_paths[$alias] = $path;
     }
 
+    /**
+     * Add a private key file path for a specific alias manually. If the alias already
+     * exists, the existing path is overwritten.
+     *
+     * @param string $alias
+     * @param string $path
+     */
     public static function addPrivateKeyPath($alias, $path)
     {
         self::$private_key_paths[$alias] = $path;
