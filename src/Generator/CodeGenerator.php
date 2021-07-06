d23f147fcd878b57fd7b0a390e7942daebd8f33f
Nathan Utama <nathan@nutama.nl>
2021-07-06 18:10:49 +0200

    fixed correct creations of import statements for Traits

--- src/Generator/CodeGenerator.php
+++ src/Generator/CodeGenerator.php
@@ -391,7 +391,7 @@ class CodeGenerator implements CodeGeneratorInterface
                 [
                     'namespace' => $class->getNamespace() . '\\' . $this->namespace,
                     'name'      => $class->getName() . $this->name_suffix,
-                    'uses'      => $this->getUniqueImports($imports),
+                    'uses'      => UniqueImports::filter($imports),
                     'methods'   => rtrim($code),
                     'username'  => get_current_user(),
                     'hostname'  => gethostname(),
@@ -650,47 +650,6 @@ class CodeGenerator implements CodeGeneratorInterface
         $this->key_registry_data = [];
 
         return true;
-    }
-
-    /**
-     * Make sure our use statements are sorted alphabetically and unique. The
-     * array_unique function can not be used because it does not take values
-     * with different array keys into account. This loop does exactly that.
-     * This is useful when a specific class name is imported and aliased as
-     * well.
-     *
-     * @param string[] $imports
-     * @return string[]
-     */
-    private function getUniqueImports(array $imports): array
-    {
-        uksort($imports, function ($a, $b) use ($imports) {
-            $alias_a = is_numeric($a) ? " as $a;" : '';
-            $alias_b = is_numeric($b) ? " as $b;" : '';
-
-            return strcmp($imports[$a] . $alias_a, $imports[$b] . $alias_b);
-        });
-
-        $unique_imports = [];
-        $next           = null;
-        do {
-            $key   = key($imports);
-            $value = current($imports);
-            $next  = next($imports);
-
-            if ($value === $next && $key === key($imports)) {
-                continue;
-            }
-
-            if ($key) {
-                $unique_imports[$key] = $value;
-                continue;
-            }
-
-            $unique_imports[] = $value;
-        } while ($next !== false);
-
-        return $unique_imports;
     }
 
     /**
